47b445f5b2c3f7f411afefab9b1fd8bc20f63465
Joby Elliott <code@byjoby.com>
2022-12-02 11:48:17 -0700

    correct names of standard groups for legacy systems

--- plugins/shared_permissions/routes/~groups/index.php.action.php
+++ plugins/shared_permissions/routes/~groups/index.php.action.php
@@ -8,5 +8,15 @@ $netid = strtolower(Context::arg('netid'));
 if ($netid) $groups = SharedPermissions::groups($netid);
 else $groups = [];
 
+// correct names of standard groups for legacy systems
+$groups = array_map(
+    function(string $group) {
+        if ($group == 'admins') return 'admin';
+        elseif($group == 'editors') return 'editor';
+        else return $group;
+    },
+    $groups
+);
+
 Context::response()->filename('groups.json');
 echo json_encode($groups);
