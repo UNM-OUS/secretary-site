b6496cee9ef76044c5848a466cc68acf303ab335
Joby Elliott <code@byjoby.com>
2022-12-02 11:16:14 -0700

    shared permissions management UI

---
+++ plugins/shared_permissions/routes/~shared_permissions/known_netids.yaml.action.php
@@ -0,0 +1,21 @@
+<?php
+
+use DigraphCMS\Context;
+use DigraphCMS_Plugins\unmous\ous_digraph_module\PersonInfo;
+use DigraphCMS_Plugins\unmous\shared_permissions\SharedPermissions;
+
+$known_netids = [];
+
+foreach (SharedPermissions::allNetids() as $netid) {
+    $known_netids[$netid] = [
+        'name' => PersonInfo::getFullNameFor($netid),
+        'groups' => SharedPermissions::groups($netid)
+    ];
+}
+
+ksort($known_netids);
+
+Context::response()->filename('known_netids.yaml');
+Context::response()->mime('text/plain');
+echo '# ' . time() . PHP_EOL;
+echo spyc_dump($known_netids);
