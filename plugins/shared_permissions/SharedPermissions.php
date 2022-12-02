28361cfdd170fd62b55c531f7c3d09e6c696783a
Joby Elliott <code@byjoby.com>
2022-12-02 11:35:57 -0700

    production bug fix

--- plugins/shared_permissions/SharedPermissions.php
+++ plugins/shared_permissions/SharedPermissions.php
@@ -2,6 +2,7 @@
 
 namespace DigraphCMS_Plugins\unmous\shared_permissions;
 
+use DigraphCMS\Datastore\DatastoreItem;
 use DigraphCMS\Datastore\DatastoreNamespace;
 use DigraphCMS\Plugins\AbstractPlugin;
 use DigraphCMS\UI\UserMenu;
@@ -29,21 +30,29 @@ class SharedPermissions extends AbstractPlugin
     public static function allNetids(): array
     {
         static $cache;
-        return $cache ?? $cache = array_values(
+        return $cache ?? $cache = array_map(
+            function (DatastoreItem $person) {
+                return $person->groupName();
+            },
             static::storage()
                 ->select()
                 ->group('grp')
-                ->fetchPairs('grp', 'grp')
+                ->fetchAll()
         );
     }
 
     public static function groups(string $netid): array
     {
         static $cache = [];
-        return $cache[$netid] ?? $cache[$netid] = array_values(static::storage()
-            ->select()
-            ->where('grp', $netid)
-            ->fetchPairs('key', 'key'));
+        return $cache[$netid] ?? $cache[$netid] = array_map(
+            function (DatastoreItem $person) {
+                return $person->key();
+            },
+            static::storage()
+                ->select()
+                ->where('grp', $netid)
+                ->fetchAll()
+        );
     }
 
     public function onStaticUrlPermissions_shared_permissions(URL $url, User $user): ?bool
